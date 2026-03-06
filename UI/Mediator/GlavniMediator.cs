da6d3b52b1b079ce976802928a42e85e6fcb23c9
Luksomir <kuridza2001kg@gmail.com>
2026-03-06 20:48:47 +0100

    UI WIP

--- UI/Mediator/GlavniMediator.cs
+++ UI/Mediator/GlavniMediator.cs
@@ -1,45 +1,46 @@
-﻿using CoWorkingManager.UI.Views;
+﻿using CoWorkingManager.Modeli;
+using CoWorkingManager.UI.Views;
 
 namespace CoWorkingManager.Mediator
 {
     public class GlavniMediator : IMediator
     {
-        private GlavniWindow main;
-        private KorisniciWindow users;
-        private LokacijeWindow locations;
-        private TipoviClanstvaWindow memberships;
-        private ResursiWindow resources;
-        private RezervacijeWindow reservations;
+        private GlavniWindow glavna;
+        private KorisniciWindow korisnici;
+        private LokacijeWindow lokacije;
+        private TipoviClanstvaWindow tipoviClanstva;
+        private ResursiWindow resursi;
+        private RezervacijeWindow rezervacije;
 
-        public void SetMain(GlavniWindow w) => main = w;
-        public void SetUsers(KorisniciWindow w) => users = w;
-        public void SetLocations(LokacijeWindow w) => locations = w;
-        public void SetMemberships(TipoviClanstvaWindow w) => memberships = w;
-        public void SetResources(ResursiWindow w) => resources = w;
-        public void SetReservations(RezervacijeWindow w) => reservations = w;
+        public void SetMain(GlavniWindow w) => glavna = w;
+        public void SetUsers(KorisniciWindow w) => korisnici = w;
+        public void SetLocations(LokacijeWindow w) => lokacije = w;
+        public void SetMemberships(TipoviClanstvaWindow w) => tipoviClanstva = w;
+        public void SetResources(ResursiWindow w) => resursi = w;
+        public void SetReservations(RezervacijeWindow w) => rezervacije = w;
 
         public void Notify(object sender, string eventCode)
         {
             switch (eventCode)
             {
                 case "OPEN_USERS":
-                    users.Show();
+                    korisnici.Show();
                     break;
 
                 case "OPEN_LOCATIONS":
-                    locations.Show();
+                    lokacije.Show();
                     break;
 
                 case "OPEN_MEMBERSHIP":
-                    memberships.Show();
+                    tipoviClanstva.Show();
                     break;
 
                 case "OPEN_RESOURCES":
-                    resources.Show();
+                    resursi.Show();
                     break;
 
                 case "OPEN_RESERVATIONS":
-                    reservations.Show();
+                    rezervacije.Show();
                     break;
             }
         }
