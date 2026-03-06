e41ac20d12fa9f9bc3bc2231d4a6f80acfa3fbb7
Luksomir <kuridza2001kg@gmail.com>
2026-03-06 22:44:23 +0100

    UI WIP 2

--- UI/Mediator/GlavniMediator.cs
+++ UI/Mediator/GlavniMediator.cs
@@ -1,45 +1,58 @@
 ﻿using CoWorkingManager.Modeli;
 using CoWorkingManager.UI.Views;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace CoWorkingManager.Mediator
 {
     public class GlavniMediator : IMediator
     {
-        private GlavniWindow glavna;
+        private GlavniWindow glavniMeni;
         private KorisniciWindow korisnici;
         private LokacijeWindow lokacije;
         private TipoviClanstvaWindow tipoviClanstva;
         private ResursiWindow resursi;
         private RezervacijeWindow rezervacije;
 
-        public void SetMain(GlavniWindow w) => glavna = w;
+        public void SetMain(GlavniWindow w) => glavniMeni = w;
         public void SetUsers(KorisniciWindow w) => korisnici = w;
         public void SetLocations(LokacijeWindow w) => lokacije = w;
         public void SetMemberships(TipoviClanstvaWindow w) => tipoviClanstva = w;
         public void SetResources(ResursiWindow w) => resursi = w;
         public void SetReservations(RezervacijeWindow w) => rezervacije = w;
 
-        public void Notify(object sender, string eventCode)
+
+        public void Notify(Window sender, string eventCode)
         {
             switch (eventCode)
             {
-                case "OPEN_USERS":
+                case "Otvori_GlavniMeni":
+                    sender.Close();
+                    glavniMeni.Show();
+                    break;
+
+                case "Otvori_Korisnike":
+                    sender.Close();
                     korisnici.Show();
                     break;
 
-                case "OPEN_LOCATIONS":
+                case "Otvori_Lokacije":
+                    sender.Close();
                     lokacije.Show();
                     break;
 
-                case "OPEN_MEMBERSHIP":
+                case "Otvori_TipoveClanstva":
+                    sender.Close();
                     tipoviClanstva.Show();
                     break;
 
-                case "OPEN_RESOURCES":
+                case "Otvori_Resurse":
+                    sender.Close();
                     resursi.Show();
                     break;
 
-                case "OPEN_RESERVATIONS":
+                case "Otvori_Rezervacije":
+                    sender.Close();
                     rezervacije.Show();
                     break;
             }
--- UI/Views/KorisniciWindow.xaml.cs
+++ UI/Views/KorisniciWindow.xaml.cs
@@ -1,13 +1,65 @@
-﻿using System.Windows;
-using CoWorkingManager.Mediator;
+﻿using CoWorkingManager.Mediator;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace CoWorkingManager.UI.Views
 {
     public partial class KorisniciWindow : Window
     {
-        public KorisniciWindow()
+        private GlavniMediator mediator;
+        string SelektovanaLokacija;
+        string SelektovanTipClanstva;
+        string SelektovanStatusaNaloga;
+
+        public KorisniciWindow(GlavniMediator mediator)
         {
-            //InitializeComponent();
+            this.mediator = mediator;
+            SelKorisnici_LokCBox.Items.Add("Lokacija");
+            SelektovanaLokacija = "Lokacija";
+            SelKorisnici_TipClCBox.Items.Add("TipClasntva");
+            SelektovanTipClanstva = "TipClasntva";
+            SelKorisnici_StatusCBox.Items.Add("StatusNaloga");
+            SelektovanStatusaNaloga = "StatusNaloga";
+            InitializeComponent();
+        }
+
+        public void Show()
+        {
+            SelKorisnici_LokCBox.SelectedIndex = 0;
+            SelKorisnici_TipClCBox.SelectedIndex = 0;
+            SelKorisnici_StatusCBox.SelectedIndex = 0;
+            base.Show();
+        }
+
+        private void SelKorisnici_LokCBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+            string selectedItem = (comboBox.SelectedItem as ComboBoxItem).Content.ToString();
+            MessageBox.Show($"Selected item: {selectedItem}");
+        }
+        private void SelKorisnici_TipClCBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+            string selectedItem = (comboBox.SelectedItem as ComboBoxItem).Content.ToString();
+            MessageBox.Show($"Selected item: {selectedItem}");
+        }
+        private void SelKorisnici_StatusCBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+            string selectedItem = (comboBox.SelectedItem as ComboBoxItem).Content.ToString();
+            MessageBox.Show($"Selected item: {selectedItem}");
+        }
+
+        //Funkcija koja vrsi pretragu
+        private void Pretrazi_Click(object sender, RoutedEventArgs e)
+        {
+            //mediator.Notify(this, "Otvori_Korisnike");
+        }
+        
+        //Funkcija za povratak na Glavni Meni
+        private void GlavniMeni_Click(object sender, RoutedEventArgs e)
+        {
+            mediator.Notify(this, "Otvori_GlavniMeni");
         }
     }
 }
